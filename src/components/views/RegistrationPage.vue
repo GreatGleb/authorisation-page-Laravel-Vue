1a88811e0a997117783216898fc4450b52b868de
GreatGleb <gwelbts@gmail>
2024-10-18 11:05:13 +0300

    add background

---
+++ src/components/views/RegistrationPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main">
+    <h1>start</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegistrationPage',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .main {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    background-image: linear-gradient(180deg, rgba(199, 124, 180, 0.4) 0%, rgba(243, 118, 116, 0.6) 50.22%, rgba(199, 124, 180, 0.4) 100%), url('~@/assets/background.jpg');
+    background-position: left center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
